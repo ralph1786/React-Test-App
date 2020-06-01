03acd716fd3588e6823b478925ef36180e69ea7f
ralph1786 <ralph1786@gmail.com>
2020-06-01 12:20:31 -0400

    inital commit

---
+++ src/components/PageOne.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function PageOne() {
+  return (
+    <div>
+      <h2>Page One</h2>
+    </div>
+  );
+}
---
+++ src/components/PageTwo.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function PageTwo() {
+  return (
+    <div>
+      <h2>Page Two</h2>
+    </div>
+  );
+}
