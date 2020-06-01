af80e77e08b14de3cbb68a39ddd1d441357a22a3
ralph1786 <ralph1786@gmail.com>
2020-06-01 12:39:35 -0400

    added image

--- src/App.js
+++ src/App.js
@@ -2,11 +2,18 @@ import React from "react";
 import { Switch, Route, Link } from "react-router-dom";
 import PageOne from "./components/PageOne";
 import PageTwo from "./components/PageTwo";
+import Logo from "./Assets/reactLogo.png";
 import "./App.scss";
 
 function App() {
   return (
     <div className="App">
+      <img
+        src={Logo}
+        alt="react logo"
+        style={{ width: "300px", height: "250px" }}
+      />
+      <br />
       <Link to="/one">One</Link>
       <Link to="/two">Two</Link>
       <Switch>
